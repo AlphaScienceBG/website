e13868b8742d5a94a7fc21200c604550c8b299ff
Hristo Goumnerov <hgoumnerov@tuta.io>
2025-05-20 17:05:48 +0300

    Standardise images and add text description

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -4,7 +4,7 @@ const AboutUs = () => {
       id="aboutus"
       className="py-20 bg-gradient-to-br from-gray-900 to-datablue-900 text-white"
     >
-      <link rel="stylesheet" href="/src/styles.css" />
+      <link rel="stylesheet" href="/styles.css" />
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold mb-8 text-center">Who we are</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
